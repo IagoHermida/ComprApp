6fda8944f56b9f6cfa5de93a7ab6f7138fc2ca55
Iago Hermida Paz <iago.hermida.paz@fpcoruna.afundacion.com>
2025-05-07 18:03:11 +0200

    Primera versión LoginActivity

--- app/src/main/java/com/example/compraapp/LoginActivity.java
+++ app/src/main/java/com/example/compraapp/LoginActivity.java
@@ -1,24 +1,56 @@
 package com.example.compraapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmailLogin, etPasswordLogin;
+    private Button btnLoginConfirm;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        etEmailLogin = findViewById(R.id.etEmailLogin);
+        etPasswordLogin = findViewById(R.id.etPasswordLogin);
+        btnLoginConfirm = findViewById(R.id.btnLoginConfirm);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLoginConfirm.setOnClickListener(v -> loginUser());
+    }
+
+    private void loginUser() {
+        String email = etEmailLogin.getText().toString().trim();
+        String password = etPasswordLogin.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Por favor, rellena todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                        startActivity(intent);
+                        finish(); // Evita que vuelva con el botón de atrás
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
